61a120fdb7c2e26b7a964cc0e1e44bfb3e1c4f15
Mahmoud Hussein <mahmod.hus@gmail.com>
2022-10-12 18:14:37 +0300

    Drawer

--- lib/task_mangement/main_task_screen.dart
+++ lib/task_mangement/main_task_screen.dart
@@ -9,39 +9,94 @@ void main(List<String> args) {
   ));
 }
 
-class MainTaskScreen extends StatelessWidget {
+class MainTaskScreen extends StatefulWidget {
   const MainTaskScreen({super.key});
 
   @override
+  State<MainTaskScreen> createState() => _MainTaskScreenState();
+}
+
+class _MainTaskScreenState extends State<MainTaskScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+  initializeTabController() {
+    tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeTabController();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title: const Text('Task Mangement'),
-          bottom: const TabBar(
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            const UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(child: Text('M')),
+                accountName: Text('Mahmoud'),
+                accountEmail: Text('Mahmod.hus@gmail.com')),
+            ListTile(
+              title: const Text('All task'),
+              subtitle: const Text('Go to all task page'),
+              leading: const Icon(Icons.list),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                tabController.animateTo(0);
+              },
+            ),
+            ListTile(
+              title: const Text('Complete task'),
+              subtitle: const Text('Go to Complete task page'),
+              leading: const Icon(Icons.done),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                tabController.animateTo(1);
+              },
+            ),
+            ListTile(
+              title: const Text('Incomplete task'),
+              subtitle: const Text('Go to Incomplete task page'),
+              leading: const Icon(Icons.cancel),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                tabController.animateTo(2);
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text('Task Mangement'),
+        bottom:  TabBar(
+          controller: tabController,
             isScrollable: true,
             unselectedLabelColor: Colors.black,
             indicatorColor: Colors.black,
-            labelColor: Colors.blue, tabs: [
-            Tab(
-              text: 'All Tasks',
-            ),
-            Tab(
-              text: 'Complete Tasks',
-            ),
-            Tab(
-              text: 'Incomplete Tasks',
-            ),
-          ]),
-        ),
-        body: const TabBarView(children: [
-          AllTaskScreen(),
-          CompleteTaskScreen(),
-          InCompleteTaskScreen(),
-        ]),
+            labelColor: Colors.blue,
+            tabs: const [
+              Tab(
+                text: 'All Tasks',
+              ),
+              Tab(
+                text: 'Complete Tasks',
+              ),
+              Tab(
+                text: 'Incomplete Tasks',
+              ),
+            ]),
       ),
+      body: TabBarView(
+        controller: tabController, 
+        children: const [
+        AllTaskScreen(),
+        CompleteTaskScreen(),
+        InCompleteTaskScreen(),
+      ]),
     );
   }
 }
